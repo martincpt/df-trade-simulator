adc4e0b4b74324c05837e869dc63dcc617282c95
martincpt <martincpt@gmail.com>
2022-09-27 15:18:47 +0200

    Update __init__.py

--- df_trade_simulator/__init__.py
+++ df_trade_simulator/__init__.py
@@ -1,2 +1,5 @@
-from df_trade_simulator.df_trade_simulator import DFTradeSimulator
+__version__ = "0.0.1"
+
+from df_trade_simulator.df_trade_simulator import DFTrade, DFTradeSimulator
+from df_trade_simulator.df_trade_simulator import Side
 from df_trade_simulator.df_trade_simulator import BUY, SELL, HODL
